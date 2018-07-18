700283c8b34c2d1c9d0d7a2c34f19a666f51ac2c
Ashutosh <ashutjha@deloitte.com>
2018-07-19 01:54:08 +0530

    rest api - post and delete completed

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,5 @@
+var bodyParser = require('body-parser');
+var urlencodedparser = bodyParser.urlencoded({extended: false});
 //dummy data
 var data = [{
     item: 'wake up'
@@ -11,10 +13,14 @@ module.exports = function(app){
     app.get('/todo', function(req, res){
         res.render('todo', {todos: data});
     });
-    app.post('/todo', function(req, res){
-
+    app.post('/todo', urlencodedparser, function(req, res){        
+        data.push(req.body);
+        res.json(data);
     });
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-
+  
   $('form').on('submit', function(){
 
       var item = $('form input');
       var todo = {item: item.val()};
-
+    
       $.ajax({
         type: 'POST',
         url: '/todo',
