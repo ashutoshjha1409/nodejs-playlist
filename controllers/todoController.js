168c340bff8b32e95cf88109b884025350734045
Ashutosh <ashutjha@deloitte.com>
2018-07-19 01:18:24 +0530

    dummy data sent to view

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,15 @@
+//dummy data
+var data = [{
+    item: 'wake up'
+},{
+    item: "code"
+}, {
+    item: "sleep"
+}];
+
 module.exports = function(app){
     app.get('/todo', function(req, res){
-
+        res.render('todo', {todos: data});
     });
     app.post('/todo', function(req, res){
 
