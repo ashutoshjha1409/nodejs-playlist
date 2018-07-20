4eecb5b6c4c4494307c2f112f8dfe241aae4ba97
Ashutosh <ashutjha@deloitte.com>
2018-07-21 00:24:50 +0530

    mlab connection

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,28 @@
 var bodyParser = require('body-parser');
 var urlencodedparser = bodyParser.urlencoded({extended: false});
+var mongoose = require('mongoose');
+//Connect with Mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://admin:admin1409@ds243441.mlab.com:43441/todo-node-0720', {
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE,
+    useMongoClient: true
+  });
+
+//Create a Schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'sleep again'}).save(function(err){
+    if(err) throw err;
+    console.log('Item saved');
+});
+
+
+
+
 //dummy data
 var data = [{
     item: 'wake up'
@@ -21,6 +44,7 @@ module.exports = function(app){
         data = data.filter(function(todo){
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
+        
         res.json(data);
     });
 };
